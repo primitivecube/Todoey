89c4ea010a0e30d0748b1208448dac997a26f3ad
Brett McDonald <bldmcdonald@gmail.com>
2019-05-12 17:08:46 +1200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Brett McDonald on 12/05/19.
+//  Copyright © 2019 Brett McDonald. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
